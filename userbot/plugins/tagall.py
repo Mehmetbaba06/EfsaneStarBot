4b4e7418b88ca6d90a707315c731ecb449e8b8bb
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-07-03 22:38:47 +0300

    Update tagall.py

--- userbot/plugins/tagall.py
+++ userbot/plugins/tagall.py
@@ -3,8 +3,8 @@
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
-# ExelonUserBot - BristolMyers
-"""Sohbette etiketlemek için bir eklenti @exelonuserbot `.all`"""
+# EfsaneUserBot - KingMehmet06
+"""Sohbette etiketlemek için bir eklenti @efsaneuserbot `.all`"""
 
 from userbot.utils import admin_cmd
 
